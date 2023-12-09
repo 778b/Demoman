5ab134979898a1dd4a0b17ca2f933ffe190aef2f
778b <kklokov@inbox.ru>
2023-12-09 21:19:55 +0400

    removed unity builds

--- Source/Demoman.Target.cs
+++ Source/Demoman.Target.cs
@@ -8,6 +8,8 @@ public class DemomanTarget : TargetRules
 	public DemomanTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
+        bUseUnityBuild = false;
+        bUsePCHFiles = false;
         DefaultBuildSettings = BuildSettingsVersion.V4;
         IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
         ExtraModuleNames.AddRange( new string[] { "Demoman" } );
